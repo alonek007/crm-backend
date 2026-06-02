be93d821bd0b8a3eeca85e44ac6d641feb440e91
monkOnkey <ayushxd69@gmail.com>
2026-06-02 23:41:08 +0530

    auth

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+function auth(req,res,next) {
+    const token = req.headers.authorization.split(" ")[1]
+
+if (!token){
+        return res.send("no token provided")
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = decoded
+        next()
+    }
+    catch(err) {
+        return res.send("invalid token")
+    }
+}
+
+
+module.exports = auth
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+require('dotenv').config()
+
 const express = require('express')
 const bcrypt = require('bcrypt')
 const pool = require('./db/db.js')
 const app = express();
-port = 3000
+port = process.env.PORT
 const z = require('zod')
+const jwt = require("jsonwebtoken")
 
 
 
@@ -93,7 +96,7 @@ console.log(hashedPassword);
 
 
 
-
+ramp1301
 
 
 
@@ -101,15 +104,48 @@ console.log(hashedPassword);
 
 
 app.post('/login', async function(req, res) {
-const {email, password} = req.body;
-   const check = await pool.query("SELECT * FROM users WHERE email = $1 ", [email])
 
-    if(check.rows.length > 0 ) {
-        res.status(400).json({
-            message: "already registered"
+    const validatelogin = zodSchema.safeParse(req.body)
+
+
+
+    if (!validatelogin.success) {
+    return res.json({
+        message: "name and password err"
+    }) 
+
+    }
+// check email 
+
+const checkemail= await pool.query("SELECT * FROM users WHERE email = $1 ", [req.body.email])
+if (checkemail.rows.length === 0 ) {
+    return res.json({
+        message: "not found "
+    })
+}
+const user = checkemail.rows[0];
+
+    const checkpass = await bcrypt.compare(req.body.password, user.password)
+    if (!checkpass) {
+        return res.json({
+            message: "password not correct"
         })
     }
+
+const payload = {
+        userId: user.id
+    }
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' })
+
+    return res.send({ message: "login successful", token })
+
+
+
+
 })
+
+
 app.listen(port, function() { 
     console.log(`connected ${port}`)
 });
